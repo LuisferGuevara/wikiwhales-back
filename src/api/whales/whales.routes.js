4800bfd13b4cac8e770079abfb23d7c366363078
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-11-20 01:01:13 +0100

    review

--- src/api/whales/whales.routes.js
+++ src/api/whales/whales.routes.js
@@ -15,7 +15,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", [isAuth], async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
     const whaleToFind = await Whale.findById(id);
